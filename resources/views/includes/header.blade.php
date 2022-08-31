de2bce75767283d73faadbc43c0c245ad7c480a0
zchronis <60366837+zchronis@users.noreply.github.com>
2022-08-31 12:35:20 -0700

    Transitioning to Bootstrap 5.2.0

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -26,14 +26,14 @@
 
         <!-- Bootstrap CSS -->
         <link 
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" 
         rel="stylesheet" 
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
         crossorigin="anonymous">
 
         <!-- Bootstrap JS -->
         <script 
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
         crossorigin="anonymous">
         </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,93 +11,89 @@
                 beds!
             </p>
         </div>
-
-        {{-- LOOP THROUGH ALL FITLERS --}}
-        <form>
-        @csrf
-            @foreach($result->values as $filter)
-                {{-- RADIO BUTTON --}}
-                @if($filter[1] == "Radio Button")
-                    <div>
-                        <p>{{$filter[0]}}</p>
-                        @foreach(array_slice($filter, 2) as $option)
-                            <a>
-                                <input onclick="" type="radio" class="btn-check typeRadio" name="{{$filter[0]}}Filter" id="{{$option}}" autocomplete="off">
-                                <label class="btn btn-outline-primary" for="{{$option}}">{{$option}}</label>
-                            </a>
-                        @endforeach
-                    </div>
-                @endif
-
-                {{-- CHECK BOX --}}
-                @if($filter[1] == "Check Box")
-                    <div class="form-check form-switch form-check-reverse">
-                        <input type="checkbox" class="form-check-input" name="{{$filter[0]}}Filter" id="{{$filter[0]}}" autocomplete="off">
-                        <label class="form-check-label" for="{{$filter[0]}}">{{$filter[0]}}</label>
-                    </div>
-                @endif
-
-                {{-- SLIDER --}}
-                @if($filter[1] == "Slider")
-                    <div class="form-group">
-                        <label for="{{$filter[0]}}">{{$filter[0]}}</label>
-                        <input type="range" class="form-control-range" name="{{$filter[0]}}Filter" id="{{$filter[0]}}" min="{{$filter[2]}}" max="{{$filter[3]}}" step="1" autocomplete="off">
-                    </div>
-                @endif
-
-                {{-- MUTLI SELECT --}}
-                @if($filter[1] == "Multiple Select")
-                    <div class="form-group">
-                        <p>{{$filter[0]}}</p>
-                        @foreach(array_slice($filter, 2) as $option)
-                            <div class="form-check form-switch form-check-reverse">
-                                <input type="checkbox" class="form-check-input" name="{{$option}}Filter" id="{{$option}}" autocomplete="off">
-                                <label class="form-check-label" for="{{$option}}">{{$option}}</label>
-                            </div>
-                        @endforeach
-                    </div>
-                @endif
-
-                {{-- DROPDOWN --}}
-                @if($filter[1] == "Drop Down")
-                    <div class="form-group">
-                        <label for="{{$filter[0]}}">{{$filter[0]}}</label>
-                        <select class="form-control" name="{{$filter[0]}}Filter" id="{{$filter[0]}}" autocomplete="off">
-                            @foreach(array_slice($filter, 2) as $option)
-                                <option value="{{$option}}">{{$option}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                @endif
-
-            @endforeach
-        </form>
-        {{-- END LOOP --}}
     </div>
-</div>
+</div>        
+<button type="button" class="btn btn-primary container d-flex justify-content-center col-2" data-bs-toggle="modal" data-bs-target="#ModalCenter">Search</button>
 
+{{-- LOOP THROUGH ALL FITLERS --}}
+<form action="" method="" enctype="">
+@csrf
+    <div class="modal fade" id="ModalCenter" tabindex="-1" role="dialog" aria-labeledby="ModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalCenterTitle">Search</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    @foreach($result->values as $filter)
+                        {{-- RADIO BUTTON --}}
+                        @if($filter[1] == "Radio Button")
+                            <div class="mb-3">
+                                <p>{{$filter[0]}}</p>
+                                @foreach(array_slice($filter, 2) as $option)
+                                    <a>
+                                        <input onclick="" type="radio" class="btn-check typeRadio" name="{{$filter[0]}}Filter" id="{{$option}}" autocomplete="off">
+                                        <label class="btn btn-outline-primary me-2" for="{{$option}}">{{$option}}</label>
+                                    </a>
+                                @endforeach
+                            </div>
+                        @endif
+                    
+                        {{-- CHECK BOX --}}
+                        @if($filter[1] == "Check Box")
+                            <div class="form-check form-switch form-check-reverse mb-3">
+                                <input type="checkbox" class="form-check-input" name="{{$filter[0]}}Filter" id="{{$filter[0]}}" autocomplete="off">
+                                <label class="form-check-label" for="{{$filter[0]}}">{{$filter[0]}}</label>
+                            </div>
+                        @endif
 
-<div class="pt-2 container">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="filter" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
-        Filter
-    </button>
+                        {{-- SLIDER --}}
+                        @if($filter[1] == "Slider")
+                            <div class="form-group mb-3">
+                                <label for="{{$filter[0]}}">{{$filter[0]}}</label>
+                                <input type="range" class="form-control-range align-middle" name="{{$filter[0]}}Filter" id="{{$filter[0]}}" min="{{$filter[2]}}" max="{{$filter[3]}}" step="1" autocomplete="off">
+                            </div>
+                        @endif
 
-    <ul class="dropdown-menu dropdown-menu-dark p-2 " aria-labelledby="filter">
-        <p>Gender</p>
-        <li id="Gender">
-            <a>
-                <input onclick="" type="radio" class="btn-check typeRadio" name="genderFilter" id="male" autocomplete="off">
-                <label class="btn btn-outline-primary" for="male">Male</label>
-            </a>
-            <a>
-                <input onclick="" type="radio" class="btn-check typeRadio" name="genderFilter" id="female" autocomplete="off">
-                <label class="btn btn-outline-primary" for="female">Female</label>
-            </a>
-        </li>
-            
-        <li><hr class="dropdown-divider"></li>
-    </ul>
-</div>
+                        {{-- MUTLI SELECT --}}
+                        @if($filter[1] == "Multiple Select")
+                            <div class="form-group mb-3">
+                                <p>{{$filter[0]}}</p>
+                                @foreach(array_slice($filter, 2) as $option)
+                                    <div class="form-check form-check-reverse">
+                                        <input type="checkbox" class="form-check-input" name="{{$option}}Filter" id="{{$option}}" autocomplete="off">
+                                        <label class="form-check-label" for="{{$option}}">{{$option}}</label>
+                                    </div>
+                                @endforeach
+                            </div>
+                        @endif
+
+                        {{-- DROPDOWN --}}
+                        @if($filter[1] == "Drop Down")
+                            <div class="form-group mb-3">
+                                <label for="{{$filter[0]}}">{{$filter[0]}}</label>
+                                <div class="d-flex col-6">
+                                    <select class="form-select mt-1" name="{{$filter[0]}}Filter" id="{{$filter[0]}}" autocomplete="off">
+                                        @foreach(array_slice($filter, 2) as $option)
+                                            <option value="{{$option}}">{{$option}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        @endif
+
+                    @endforeach
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Continue</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+{{-- END LOOP --}}
 
 
 @endsection
