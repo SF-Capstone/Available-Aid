6517d7f56568d56272a61e8719e9a4a5a86cdaa1
Bishop <59637502+Iwasson@users.noreply.github.com>
2022-08-31 12:57:26 -0700

    Loading bootstrap locally
    A local version of bootstrap has been loaded into the project. This will mean that we no longer need to rely on the CDN and we have a stable version of bootstrap

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -25,18 +25,10 @@
         </style>
 
         <!-- Bootstrap CSS -->
-        <link 
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" 
-        rel="stylesheet" 
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
-        crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
         <!-- Bootstrap JS -->
-        <script 
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
-        crossorigin="anonymous">
-        </script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
         <!-- Axios -->
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
