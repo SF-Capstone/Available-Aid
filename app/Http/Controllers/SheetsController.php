62aceaf5d324bc7af7cd877651bcd1311ec0d761
Bishop <59637502+Iwasson@users.noreply.github.com>
2022-09-12 12:22:14 -0700

    Added working filter functionality
    The submission of the search form will now display the shelters that match the criteria

--- app/Http/Controllers/SheetsController.php
+++ app/Http/Controllers/SheetsController.php
@@ -6,7 +6,7 @@ use Google_Service_Sheets;
 use Google\Client;
 use Google\Service\Drive;
 use Google\Service\Sheets;
-
+use Illuminate\Http\Request;
 
 class SheetsController extends Controller
 {
@@ -19,7 +19,7 @@ class SheetsController extends Controller
         $service = new Google_Service_Sheets($client);
 
 
-        $range = 'Filters!E2:Z100';
+        $range = 'Filters!A2:Z100';
 
         try {
             $result = $service->spreadsheets_values->get($spreadsheetId, $range);
@@ -30,22 +30,61 @@ class SheetsController extends Controller
         }
     }
 
-    public function getShelterInfo() {
+    public function getShelterInfo(Request $request) {
+        $activeFilters = array_slice($request->all(), 1);
+        
         $spreadsheetId = Env('SPREADSHEET_ID');
         $client = new Client();
         $client->setAuthConfig(storage_path('app/serviceCredentials.json'));
         $client->addScope(Drive::DRIVE);
         $service = new Google_Service_Sheets($client);
 
-        $infoRange = 'Info!A2:Z';
+        $infoRange = 'Info!A:Z';
 
         try{
             $shelterResultInfo = $service->spreadsheets_values->get($spreadsheetId, $infoRange);
+            $headers = $shelterResultInfo['values'][0];
+            $shelterResult = array_slice($shelterResultInfo['values'], 1);
             $lastFormInput = array();
-            foreach ($shelterResultInfo->values as $shelter) {
+
+            $mappedResults = array(); 
+            
+            foreach($shelterResult as $shelter) {
+                $temp = array();
+                foreach($shelter as $key => $value) {
+                    $temp[$headers[$key]] = $value;
+                }
+                $mappedResults[] = $temp;
+            }
+        
+            foreach ($mappedResults as $shelter) {
+
+                foreach($activeFilters as $key => $value) {
+                    //strip out the underscores in the key
+                    $key = str_replace('_', ' ', $key);
+
+                    //if gender is unspecified, then skip it
+                    if($key == "Gender" && $value == "Unspecified") {
+                        continue;
+                    } else {
+                        //convert on values to yes
+                        if($value == "on") {
+                            $value = "Yes";
+                        }
+                        //check if the shelter has unspecified as a value, skip it if it does
+                        if($shelter[$key] == "Unspecified") {
+                            continue;
+                        }
+                        //if the shelter has the filter, and the values do not match, then skip it
+                        if(array_key_exists($key, $shelter) && $shelter[$key] != $value) {
+                            continue 2;
+                        }
+                    }
+                }
+
                 $result = array();
 
-                $lastRowHeaderRange = $shelter[0] . '!A1:Z1';
+                $lastRowHeaderRange = $shelter["Shelter Name"] . '!A1:Z1';
                 $lastRowHeader = $service->spreadsheets_values->get($spreadsheetId, $lastRowHeaderRange)->values[0];
 
                 //loop through all of the headers and get the index of the row that contains the word bed case insensitive
@@ -64,18 +103,19 @@ class SheetsController extends Controller
                     }
                 }
                 
-                $lastRowRange = $shelter[0] . '!A' . $shelter[3] . ':C' . $shelter[3];
+                $lastRowRange = $shelter["Shelter Name"] . '!A' . $shelter["Most Recent entry row"] . ':C' . $shelter["Most Recent entry row"];
                 $mostRecent = $service->spreadsheets_values->get($spreadsheetId, $lastRowRange)->values[0];
 
                 $result['beds'] = $mostRecent[$bedIndex];
                 $result['timestamp'] = date('g:ia m/d/Y', strtotime($mostRecent[$timestampIndex]));
-                $result['shelter'] = $shelter[0];
-                $result['address'] = $shelter[1];
-                $result['phone'] = $shelter[2];
+                $result['shelter'] = $shelter["Shelter Name"];
+                $result['address'] = $shelter["Location"];
+                $result['phone'] = $shelter["Contact Info"];
 
 
                 $lastFormInput[] = $result;
             }
+
             return view('results', compact('shelterResultInfo' , 'lastFormInput'));
         } catch(Exception $e) {
             // TODO(developer) - handle error appropriately
