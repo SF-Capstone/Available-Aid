e5b29d4a3f6b162544930886d67432916ae0f2bf
User name <68046886+DebuggerDan@users.noreply.github.com>
2022-09-23 00:09:48 -0700

    Grammatical fix of title + composer pkg updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 <div class="container d-flex bootstrap-grid mb-2 mt-4 text-center mx-auto">
     <div class="row row1 mx-auto">
         <div class="col-lg-12 col-md-12 col-sm-12 mx-auto">
-            <h1>Welcome to Avalible Aid</h1>
+            <h1>Welcome to Available Aid!</h1>
             <p>
                 beds!
             </p>
