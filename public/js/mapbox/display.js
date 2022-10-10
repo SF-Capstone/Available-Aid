94a7b5471022bd6a12384f49f6d6b5395abfa466
Matthew Pickett <mpickett@pdx.edu>
2022-10-09 23:19:39 -0700

    Added additional options to get current location

--- public/js/mapbox/display.js
+++ public/js/mapbox/display.js
@@ -14,9 +14,17 @@ let directions = new MapboxDirections({
     accessToken: mapboxgl.accessToken,
     interactive: false,
     //Removes origin/destination, but keeps instructions
-    controls: {
-        inputs: false,
-        instructions: true
-  }
+//     controls: {
+//         inputs: false,
+//         instructions: true
+//   }
 });
 map.addControl(directions, 'top-left');
+
+
+let geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+    })
+map.addControl(geolocate);
