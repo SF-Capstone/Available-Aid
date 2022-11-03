90c924dfbfba0972bfff32c0103ecdca017d9d32
Iwasson <59637502+Iwasson@users.noreply.github.com>
2022-11-03 16:21:09 -0700

    Consistency pass (#21)
    * First pass on main page
    
    * Completed consistency pass
    
    Made each page utilize the projects color-scheme and styling.
    
    * Further formatting and styling
    
    * Fixed typo
    
    * Making Edits
    
    New logo, and styling edits have been made to the general website
    
    Co-authored-by: zchronis <60366837+zchronis@users.noreply.github.com>

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -12,6 +12,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         
+
         <title>Portland Aid Guide</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('image/logo4.png') }}">
 
