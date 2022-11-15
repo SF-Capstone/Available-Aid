ae68256b7a41b037008ebd295657f3520a08a634
Matthew Pickett <91637250+matt-pickett@users.noreply.github.com>
2022-11-14 18:04:49 -0800

    Minor map UI adjustments (#23)
    * Getting coordinate data out of the browser
    
    Console logs the users current location. This will prompt the user to allow their location data being used
    
    * Rebasing off of develop
    
    Updating the branch to now have the changes from develop. Outputs current coords to the console for use in search filters
    
    * Setting up mapbox
    
    Getting basic mapbox displaying and setting up the files needed to run everything
    
    * Adding navigation controls
    
    Added navigation controls to the map, allowing users to input a starting and finishing location and choosing which mode of transportation they want to use. Need to investigate if there is a way to get trimet data for transit lines
    
    * Generate map of shelter locations
    
    * Add shelter marker popup features
    
    * Make route directions responsive
    
    * Integrate map with info page
    
    * Added additional options to get current location
    
    * Additional styling and coordinating map colors to website
    
    * Minor bug fixes
    
    Co-authored-by: Bishop <59637502+Iwasson@users.noreply.github.com>

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -5,25 +5,24 @@
 {{-- shelter information --}}
 
 <div class="container d-flex bootstrap-grid mb-2 mt-2 text-center mx-auto">
-    <div class="card mb-3 col-lg-12 col-md-12 col-sm-12 mx-auto card-width">
-        <img src="{{ route('getImage', ['imageName' => $result['Shelter Name']]) }}" class="card-img-top results" alt="Shelter Image">
+
+    <div class="card mb-3 col-lg-12 col-md-12 col-sm-12 mx-auto">
+
+
+        <img src="{{ route('getImage', ['imageName' => $result['Shelter Name']]) }}"class="card-img-top informationImages" alt="Shelter Image">
 
         <div class="card-body">
             <h5 class="card-title">{{ $result['Shelter Name'] }}</h5>
-            <p class="card-text" style="margin-bottom: -0.25em;">{{ $result['Location'] }}</p>
-            <a class="card-text telephone" href="tel:{{ $result['Contact Info'] }}"> {{ $result['Contact Info'] }}</a>
-            <h5 class="mt-3 card-text">Available Beds: {{ $result['Beds'] }}</h5>
+            <p class="card-text">{{ $result['Location'] }}</p>
+            <a class="card-text" href="tel:{{ $result['Contact Info'] }}"> {{ $result['Contact Info'] }}</a>
+            <h5 class="card-text">Avalible Beds: {{ $result['Beds'] }}</h5>
             <p class="card-text"><small class="text-muted">Last Updated: {{ $result['Timestamp'] }}</small></p>
-            <hr>
             @foreach(array_slice($result, 6) as $key => $info)
-                @if($info != 'No' && $key != "Row Number" && $info != NULL && $info != "NA")
-                <p class="card-text text-start"><strong>{{ $key }}:</strong> {{ $info }}</p>
-                @endif
+            @if($info != 'No')
+            <p class="card-text">{{ $key }}: {{ $info }}</p>
+            @endif
             @endforeach
-            <hr>
-            {{-- go back button --}}
-            <a href="{{ url()->previous() }}" class="btn btn-primary">Go Back</a>
-            <a class="btn btn-primary ms-2" href="{{ route('mapView', ['shelterRow' => $result['Row Number'], 'shelterName' => Str::slug($result['Shelter Name'], '-') ]) }}">Get Directions</a>
+            <a class="btn btn-primary" href="{{ route('mapView', ['shelterRow' => $result['Row Number'], 'shelterName' => Str::slug($result['Shelter Name'], '-') ]) }}">Get Directions</a>
         </div>
     </div>
 </div>
